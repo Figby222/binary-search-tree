1054840bfa31c2a923e41fcd92630d9cdf44dacc
Figby222 <figby222@outlook.com>
2024-03-23 04:24:21 -0700

    Add prettyPrint()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import Tree from './modules/tree.js'
 import { buildTree, balanceTree } from './modules/tree.js';
 // console.log(mergeSort([24, 12, 51, 33, 22, 1, 35, 21]));
-console.log(buildTree([24, 12, 51, 33, 22, 1, 35, 21]));
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+const node = buildTree([24, 12, 51, 33, 22, 1, 35, 21])
+
+console.log(prettyPrint(node));
