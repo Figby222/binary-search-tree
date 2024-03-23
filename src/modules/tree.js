7c1536547279602fbc8d65c3df771e2bf5501aa9
Figby222 <figby222@outlook.com>
2024-03-23 03:50:22 -0700

    Add buildTree()

--- src/modules/tree.js
+++ src/modules/tree.js
@@ -1,5 +1,17 @@
+import mergeSort from './modules/merge-sort.js'
+
 const Tree = function(array) {
     return {
         root: buildTree(array),
     }
 }
+
+function buildTree(array) {
+    const sortedArray = mergeSort(array);
+
+    const root = balanceTree(sortedArray);
+
+    return root;
+}
+
+export default Tree;
