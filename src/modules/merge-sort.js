855f35e519c8f2af2a43db40ac5b9c323c2d9103
Figby222 <figby222@outlook.com>
2024-03-23 03:34:47 -0700

    Add sortArrays()

--- src/modules/merge-sort.js
+++ src/modules/merge-sort.js
@@ -3,8 +3,8 @@ function mergeSort(array) {
         return array;
     }
 
-    const arrayLeft = mergeSort(array.slice(0, array.length() / 2));
-    const arrayRight = mergeSort(array.slice(array.length() / 2));
+    const arrayLeft = mergeSort(array.slice(0, array.length / 2));
+    const arrayRight = mergeSort(array.slice(array.length / 2));
 
     const sortedArray = sortArrays(arrayLeft, arrayRight);
 
@@ -13,7 +13,31 @@ function mergeSort(array) {
 }
 
 function sortArrays(arrayLeft, arrayRight) {
-    
+    let leftI = 0;
+    let rightI = 0;
+    let sortedI = 0;
+    let sortedArray = [];
+
+    while(leftI < arrayLeft.length || rightI < arrayRight.length) {
+        if (arrayLeft[leftI] < arrayRight[rightI] || rightI >= arrayRight.length) {
+            sortedArray[sortedI] = arrayLeft[leftI];
+            sortedI++;
+            leftI++;
+        } else if (arrayLeft[leftI] > arrayRight[rightI] || leftI >= arrayLeft.length) {
+            sortedArray[sortedI] = arrayRight[rightI];
+            sortedI++;
+            leftI++;
+        } else if (arrayLeft[leftI] === arrayRight[rightI]) {
+            sortedArray[sortedI] = arrayLeft[leftI];
+            leftI++;
+            rightI++;
+            sortedI++;
+        } else {
+            break;
+        }
+    }
+
+    return sortedArray;
 }
 
 export default mergeSort;
